c23c7be34b3ef8b9687b75450f428f1c265520df
KhuiShing <code4life.escr@gmail.com>
2023-07-31 00:57:36 +0630

    31/7/2023

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <h1 class="text-center bg-primary text-white p-3">
+      {{ title }}'sTodo Lists
+    </h1>
+    <div class="container">
+
+      <div class="row">
+        <div class="col fs-3 mb-4">Tasks</div>
+        <div class="col-2 fs-3 mb-4">Done</div>
+      </div>
+
+      <div class="row" v-for="(task, key) in tasks" :key="key">
+
+        <div class="col" 
+          :class="task.done ? 'line-text' : ''">{{ task.action }}</div>
+
+        <div class="col-2">
+          <input type="checkbox" v-model="task.done">
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    name: "App",
+    data: () => ({
+      title:'HKS',
+      tasks:[
+        {action: "buy new phone", done:false},
+        {action: "buy new shoe",  done:true},
+        {action: "Call Aung Aung", done:false},
+        {action: "buy new shirt", done:false},
+      ]
+    })
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.line-text{
+  text-decoration: line-through;
 }
 </style>
